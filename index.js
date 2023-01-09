8d60e090f791c6a60ca928e593c51a2bc27a9cb5
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-09 11:54:49 +0000

    my listings route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const listing = require("./routes/listing");
 const users = require("./routes/users");
 const user = require("./routes/user");
 const auth = require("./routes/auth");
-// const my = require("./routes/my");
+const my = require("./routes/my");
 const messages = require("./routes/messages");
 // const expoPushTokens = require("./routes/expoPushTokens");
 const helmet = require("helmet");
@@ -24,7 +24,7 @@ app.use("/api/listings", listings);
 app.use("/api/user", user);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
-// app.use("/api/my", my);
+app.use("/api/my", my);
 // app.use("/api/expoPushTokens", expoPushTokens);
 app.use("/api/messages", messages);
 
